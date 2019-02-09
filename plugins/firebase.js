9f6a9e69b4b1cf27f1b0f3420ed25f01c42424f7
yumuranaoki <yumura@collaborex01.com>
2019-02-10 00:30:00 +0900

    modify firebase configuration

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: $APIKEY,
-    authDomain: $AUTHDOMAIN,
-    databaseURL: $DATABASEURL,
-    projectId: $PROJECTID,
-    storageBucket: $STORAGEBUCKET,
-    messagingSenderId: $MESSAGESENDERID,
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
   })
   
 }
